746eb9d48838b917f7cc9580436e0d86e2a2e78a
Yeho Gwon <yeho.gwon@postech.ac.kr>
2025-03-27 16:24:11 +0900

    2025.03.27-16:24:11 ~ Dotfiles Pushed 🚀

--- scripts/conda_init.sh
+++ scripts/conda_init.sh
@@ -1,15 +1,24 @@
-shell_name=$SHELL
+shell_name=$(basename $SHELL)
 
 # >>> conda initialize >>>
-if [ -x "$HOME/miniconda3/bin/conda" ]; then
-    CONDA_INSTALLED_PATH="$HOME/miniconda3"
-elif [ -x "/opt/ohpc/pub/apps/anaconda3/bin/conda" ]; then
-    CONDA_INSTALLED_PATH="/opt/ohpc/pub/apps/anaconda3"
-fi
+_CANDIDATE_PATHS=(
+    "/usr/local/Caskroom/miniconda" # macos (homebrew)
+    "$HOME/miniconda3" # linux
+    "/opt/ohpc/pub/apps/anaconda3" # linux (ohpc)
+)
+
+for CANDIDATE_PATH in "${_CANDIDATE_PATHS[@]}"; do
+    if [ -d "$CANDIDATE_PATH" ]; then
+        CONDA_INSTALLED_PATH="$CANDIDATE_PATH"
+        break
+    fi
+done
+
+unset _CANDIDATE_PATHS
 
 if [ -n "$CONDA_INSTALLED_PATH" ]; then
     if [ -x "$CONDA_INSTALLED_PATH/bin/conda" ]; then
-        __conda_setup="$("$CONDA_INSTALLED_PATH/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+        __conda_setup="$("$CONDA_INSTALLED_PATH/bin/conda" 'shell.${shell_name}' 'hook' 2> /dev/null)"
         if [ $? -eq 0 ]; then
             eval "$__conda_setup"
         else
