dcb95f45177e19a1b4da53e7c55b3f12d7798d93
Yeho Gwon <yeho.gwon@postech.ac.kr>
2025-11-01 13:57:23 +0900

    resolve formatting issue

--- scripts/common_rc.sh
+++ scripts/common_rc.sh
@@ -110,7 +110,7 @@ wwatch() {
   # main loop
   while (( !interrupted )); do
     printf '\033[H\033[2J'
-    print_yellow 'wwatch :: %s :: %s\n' "$(hostname)" "$(date)"
+    print_yellow "wwatch :: $(hostname) :: $(date)"
     printf '\n'
     eval "$@"
     for ((i=0; i<interval && !interrupted; i++)); do sleep 1; done
