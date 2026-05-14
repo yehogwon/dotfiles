044388561ffd2abc777afa4e1b7874aaf378c196
Yeho Gwon <yeho.gwon@postech.ac.kr>
2026-05-15 00:13:35 +0900

    show su target user in pane title
    Detect su anywhere on the pane's tty, not just when pane_current_command is
    su itself -- once the child shell is the foreground process, pcc reports the
    shell, so scan the tty's process list to surface the target user.

--- tmux/scripts/pane_title.sh
+++ tmux/scripts/pane_title.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Render the pane-border title.
-# If pane_current_command is an ssh-like process, show the remote host;
-# otherwise show the basename of the current working directory.
+# If pane_current_command is an ssh-like process, show the remote host.
+# Else if su is running on the pane's tty (including the common case where
+# pane_current_command is the child shell spawned by su), show the target
+# user as "su:<user>".
+# Otherwise show the basename of the current working directory.
 #
 # Usage: pane_title.sh <pane_tty> <pane_current_command> <pane_active 0|1>
 #                      <pane_path>
@@ -18,10 +21,13 @@ c_active_bg=$6
 c_inactive_fg=$7
 c_assh_bg=$8
 
-host=""
+tty_name=${pane_tty##*/}
+label=""
+active_bg=$c_active_bg
+
 case "$pcc" in
     ssh|mosh-client|autossh)
-        ssh_args=$(ps -t "${pane_tty##*/}" -o args= 2>/dev/null | awk '
+        ssh_args=$(ps -t "$tty_name" -o args= 2>/dev/null | awk '
             {
                 cmd = $1
                 sub(/.*\//, "", cmd)
@@ -43,7 +49,8 @@ case "$pcc" in
                     -*)
                         shift; continue ;;
                     *)
-                        host=${1##*@}
+                        label=${1##*@}
+                        active_bg=$c_assh_bg
                         break ;;
                 esac
             done
@@ -51,12 +58,43 @@ case "$pcc" in
         ;;
 esac
 
-if [ -n "$host" ]; then
-    label=$host
-    active_bg=$c_assh_bg
-else
+# su keeps a child shell as the pane's foreground process, so pcc won't
+# announce it -- scan the tty's process list instead.
+if [ -z "$label" ]; then
+    su_args=$(ps -t "$tty_name" -o args= 2>/dev/null | awk '
+        {
+            cmd = $1
+            sub(/.*\//, "", cmd)
+            if (cmd == "su") {
+                print
+                exit
+            }
+        }
+    ')
+    if [ -n "$su_args" ]; then
+        # shellcheck disable=SC2086
+        set -- $su_args
+        shift  # drop program name
+        target="root"
+        while [ $# -gt 0 ]; do
+            case "$1" in
+                # su flags that take a value
+                -c|--command|-g|--group|-G|--supp-group|-s|--shell|--session-command)
+                    shift; [ $# -gt 0 ] && shift; continue ;;
+                -*)
+                    shift; continue ;;
+                *)
+                    target=$1
+                    break ;;
+            esac
+        done
+        label="su:$target"
+        active_bg=$c_assh_bg
+    fi
+fi
+
+if [ -z "$label" ]; then
     label=${pane_path##*/}
-    active_bg=$c_active_bg
 fi
 
 if [ "$pane_active" = "1" ]; then
